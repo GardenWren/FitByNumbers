af12f285eaedbd087de8695b3d55e80c3c429e96
GardenWren <garden.paquin@gmail.com>
2017-06-22 17:19:08 -0400

    added xcode project

---
+++ Fit By Numbers/Fit By Numbers/calc.swift
@@ -0,0 +1,15 @@
+//
+//  calc.swift
+//  Fit By Numbers
+//
+//  Created by Garden Wren Paquin on 6/22/17.
+//  Copyright © 2017 Garden Wren Paquin. All rights reserved.
+//
+
+import Foundation
+
+func TDEECalc(age: Int, gender: String, weight: Double, Height: Double){
+    var TDEE: Int = 0
+    TDEE += 2
+    
+}
